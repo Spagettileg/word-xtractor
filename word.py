e351a99e94e2e29c5d50a80b660d48a3916ea3dc
Ubuntu <ubuntu@ip-172-31-42-205.eu-west-1.compute.internal>
2020-12-14 08:28:11 +0000

    Notes to user added to help understand purpose of the app.

--- word.py
+++ word.py
@@ -2,3 +2,21 @@
 Produce a list of the most frequent interesting words, along with a summary
 table showing where those words appear (sentences and documents)
 """
+
+import os
+from collections import Counter
+import pprint # enables print of notes to user. displays txt files too
+
+def print_notes():
+    # Note to user on purpose of this app
+    
+    star = '*'.center(80, '*')
+    print(star)
+    message = 'Select document number, from 1 to 6'
+    print('*' + message.center(78) + '*')
+    message = 'Today, we can find the 10 most common words used.'
+    print('*' + message.center(78) + '*')
+    message = 'All returned words sourced from this apps directory.'
+    print('*' + message.center(78) + '*')
+    print(star + '\n') # New line
+    
